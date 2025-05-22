b2e3b82e6ae51e559d56e370160d9b2c345f3fb6
Roman Hood <roman.hood@prizepicks.com>
2025-05-22 12:19:52 -0400

    remove snacks exploror, force kanagawa

--- nvim/.config/nvim/lua/plugins/my-plugins.lua
+++ nvim/.config/nvim/lua/plugins/my-plugins.lua
@@ -1,6 +1,62 @@
 return {
   { "tpope/vim-fugitive" }, -- Git commands
   { "tpope/vim-rails" }, -- Rails commands & navigation
+  { "tpope/vim-vinegar" },
+  { "thoughtbot/vim-rspec" },
+  { "leafOfTree/vim-svelte-plugin" },
+  { "MaxMEllon/vim-jsx-pretty" },
+  { "tpope/vim-rails" },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require("kanagawa").setup({
+        compile = true,
+        dimInactive = true,
+      })
+      vim.cmd("colorscheme kanagawa")
+    end,
+    build = function()
+      vim.cmd("KanagawaCompile")
+    end,
+  },
+  { "tpope/vim-fugitive" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      context_commentstring = {
+          enable = true,
+          enable_autocmd = false,
+      },
+      ensure_installed = {
+          "markdown",
+          "tsx",
+          "typescript",
+          "javascript",
+          "toml",
+          "c_sharp",
+          "json",
+          "yaml",
+          "rust",
+          "css",
+          "html",
+          "lua",
+      },
+      rainbow = {
+          enable = true,
+          disable = { "html" },
+          extended_mode = false,
+          max_file_lines = nil,
+      },
+      autotag = { enable = true },
+      incremental_selection = { enable = true },
+      indent = { enable = true },
+    }
+  },
+	{ "windwp/nvim-ts-autotag", after = "nvim-treesitter" },
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
